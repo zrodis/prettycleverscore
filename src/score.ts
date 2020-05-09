0e464ddf70e76a64525557708fcbec65b90983e0
Zach Rodis <zach@Zachs-MacBook-Air.local>
2020-05-09 12:13:42 -0400

    calculate the blue box total

--- src/score.ts
+++ src/score.ts
@@ -1,7 +1,18 @@
 interface BlueSelection {
     quantity: number
 }
+function getPrevQuantity(currentQuantity) {
+    if (currentQuantity <= 0) return 0
 
-export const calculateBlue = (selection: BlueSelection) => {
-    return 0
+    return currentQuantity - 1
+}
+
+export const calculateBlue = ({ quantity }: BlueSelection) => {
+    let total = 0
+    let prevScore = 0
+    for (let i = 0; i <= quantity; i++) {
+        total = prevScore + getPrevQuantity(i)
+        prevScore = prevScore === 0 ? 1 : total
+    }
+    return total
 }
