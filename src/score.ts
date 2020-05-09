d9e0e66f3e64aaa272d8806df4ddf52378a672f0
Zach Rodis <zach@Zachs-MacBook-Air.local>
2020-05-09 12:24:17 -0400

    caclulate the green boxes

--- src/score.ts
+++ src/score.ts
@@ -1,4 +1,4 @@
-interface BlueSelection {
+interface Selection {
     quantity: number
 }
 function getPrevQuantity(currentQuantity) {
@@ -7,12 +7,22 @@ function getPrevQuantity(currentQuantity) {
     return currentQuantity - 1
 }
 
-export const calculateBlue = ({ quantity }: BlueSelection) => {
+export const calculateBlue = ({ quantity }: Selection): number => {
     let total = 0
     let prevScore = 0
     for (let i = 0; i <= quantity; i++) {
         total = prevScore + getPrevQuantity(i)
         prevScore = prevScore === 0 ? 1 : total
+    }
+    return total
+}
+
+export const calculateGreen = ({ quantity }: Selection): number => {
+    let total = 0
+    let modifier = 0
+    for (let i = 0; i <= quantity; i++) {
+        total += modifier
+        modifier++
     }
     return total
 }
